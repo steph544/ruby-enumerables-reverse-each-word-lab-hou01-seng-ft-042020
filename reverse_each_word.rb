4d48477c80e3e46720cc6e3ddc50e94b1bf30496
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-28 20:45:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,17 @@
 require 'spec_helper'
 
 def reverse_each_word(string)
- string = string.split('')
  reversed_string = []
  
  string.each do |char|
-  reversed_string.push(char)
+  reversed_string.push(char.reverse)
  end
  
  return reversed_string.join
 end
 
-make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
+# make_sandwich("Creamy peanut butter", "glittering sense of accomplishment") { |b| "#{b.reverse} on #{"bread".reverse}" }
 
 
- expected: "olleH ,ereht dna woh era ?uoy"
-            got: "Hello there, and how are you?"
+# expected: "olleH ,ereht dna woh era ?uoy"
+#             got: "Hello there, and how are you?"
