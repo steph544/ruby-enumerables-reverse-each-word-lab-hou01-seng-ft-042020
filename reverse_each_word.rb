25fb0428e4adad2399f90b8c1d123d0768408998
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-28 21:03:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -14,3 +14,14 @@ end
 
 # expected: "olleH ,ereht dna woh era ?uoy"
 #             got: "Hello there, and how are you?"
+
+def reverse_each_word(string)
+ string = string.split
+ reversed_string = []
+ 
+ string.collect do |char|
+  reversed_string.push(char.reverse)
+ end
+ 
+ return reversed_string.join(' ')
+end
