738c1174cbb815506b5c952a2ce664ebbeda7252
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-28 20:40:00 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+def reverse_each_word(string)
+ string = string.split('')
+ reversed_string = []
+ 
+ string.each do |char|
+  reversed_string.push(char)
+ end
+ 
+ return reversed_string.join
+end
+
+
+ expected: "olleH ,ereht dna woh era ?uoy"
+            got: "Hello there, and how are you?"
